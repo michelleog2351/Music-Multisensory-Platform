5e6c263c1d105cf0d47699b62e83d12880a8067d
michelleog2351 <L00176523@atu.ie>
2025-12-12 16:11:52 +0000

    [WIP]: add exception classes and directories to cater for service connection is lost to repo
    add services class to repo to integrate app services #11

---
+++ MusicBased_IOT_Platform/MusicBased_IOT_Platform/MusicBased_IOT_Platform/Exceptions/FitbitApplicationException.cs
@@ -0,0 +1,6 @@
+﻿namespace MusicBased_IOT_Platform.Exceptions
+{
+    public class FitbitApplicationException
+    {
+    }
+}
---
+++ MusicBased_IOT_Platform/MusicBased_IOT_Platform/MusicBased_IOT_Platform/Services/SpotifyService.cs
@@ -0,0 +1,6 @@
+﻿namespace MusicBased_IOT_Platform.Services
+{
+    public class SpotifyService
+    {
+    }
+}
