63ff9ab1d78047b5b2bef918b4373d7a8d334ee2
michelleog2351 <L00176523@atu.ie>
2026-01-22 12:09:22 +0000

    clean up model classes

--- MusicBased_IOT_Platform/MusicBased_IOT_Platform/MusicBased_IOT_Platform/Model/Album.cs
+++ MusicBased_IOT_Platform/MusicBased_IOT_Platform/MusicBased_IOT_Platform/Model/Album.cs
@@ -1,14 +1,7 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using SpotifyApp.Model;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
 using System.Text.Json.Serialization;
-using System.Threading.Tasks;
 
 namespace MusicBased_IOT_Platform.Model
 {
@@ -159,7 +152,7 @@ namespace MusicBased_IOT_Platform.Model
         /// Tracks
         /// </summary>
         [JsonPropertyName("items")]
-        public List<Track>? Tracks{ get; set; }
+        public List<Track>? Tracks { get; set; }
 
         /// <summary>
         /// Limit
--- MusicBased_IOT_Platform/MusicBased_IOT_Platform/MusicBased_IOT_Platform/Model/Releases.cs
+++ MusicBased_IOT_Platform/MusicBased_IOT_Platform/MusicBased_IOT_Platform/Model/Releases.cs
@@ -1,13 +1,7 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using MusicBased_IOT_Platform.Model;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Text.Json.Serialization;
-using System.Threading.Tasks;
 
 namespace MusicBased_IOT_Platform.Model
 {
