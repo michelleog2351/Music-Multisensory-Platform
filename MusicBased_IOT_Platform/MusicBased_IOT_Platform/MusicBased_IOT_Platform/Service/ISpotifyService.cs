589a58732d9e0f07fef0870a6e1fa3de2c26feef
michelleog2351 <L00176523@atu.ie>
2026-01-24 22:28:31 +0000

    fix firmat issue within the spotify service class #37

--- MusicBased_IOT_Platform/MusicBased_IOT_Platform/MusicBased_IOT_Platform/Service/ISpotifyService.cs
+++ MusicBased_IOT_Platform/MusicBased_IOT_Platform/MusicBased_IOT_Platform/Service/ISpotifyService.cs
@@ -1,8 +1,12 @@
 ﻿
+
 namespace MusicBased_IOT_Platform.Services
 {
     public class ISpotifyService : SpotifyService
     {
+        public ISpotifyService(HttpClient http) : base(http)
+        {
+        }
 
         /// <summary>
         internal bool TestDataConnection()
