46a52a5b2fe37c783035f4ba4f04d63c035a7206
michelleog2351 <L00176523@atu.ie>
2026-03-06 23:36:04 +0000

    chore(registration): [WIP]: bugfix to loginmodel #93

---
+++ MusicBased_IOT_Platform/MusicBased_IOT_Platform/MusicBased_IOT_Platform/Models/LoginModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MusicBased_IOT_Platform.Models
+{
+    public class LoginModel
+    {
+        [Required]
+        public string Username { get; set; } = string.Empty;
+
+        [Required]
+        public string Password { get; set; } = string.Empty;
+
+        public bool RememberMe { get; set; }
+    }
+}
