9a1ff8b4dc5411b0ca57954f99499acf4acbbcfc
michelleog2351 <L00176523@atu.ie>
2026-03-21 18:27:39 +0000

    ci(workflow): [WIP] remove Data folder (AppDBContext) from .gitignore #76

---
+++ MusicBased_IOT_Platform/MusicBased_IOT_Platform/MusicBased_IOT_Platform/Data/AppDBContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MusicBased_IOT_Platform.Models;
+
+namespace MusicBased_IOT_Platform.Data
+{
+    public class AppDBContext(DbContextOptions<AppDBContext> options) : DbContext(options)
+    {
+        public DbSet<UserAccount> Users { get; set; }
+    }
+}
